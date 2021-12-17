4711ac70e9f0a68f446d0531b50acddf4024b953
lolly <gratapaula@gmail.com>
2021-12-17 11:18:19 -0600

    update on total

--- public/js/main.js
+++ public/js/main.js
@@ -143,5 +143,5 @@ function updateCartTotal() {
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total + '.00'
 }
